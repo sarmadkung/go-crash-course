2a18c6ad41727efe86a47b80f3a06b0c47da74e0
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2026-09-09 00:01:08 +0500

    Add lesson 5 on interfaces and composition; implement Animal interface with Dog and Cat types

---
+++ lesson5.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// Lesson 5 — Interfaces & Composition ⭐⭐⭐
+
+// This is one of the most important lessons in Go.
+// Coming from TypeScript, Java, C#, or traditional OOP, you need to slightly change your mental model.
+// In Go:
+// We don't primarily use inheritance to share behavior.
+// We use interfaces and composition.
+
+// Interface
+
+// An interface defines a set of behaviors.that a type can implement. An interface defines a contract that a type must fulfill in order to be considered as implementing that interface. In Go, interfaces are satisfied implicitly, meaning that a type does not need to explicitly declare that it implements an interface. Instead, if a type has the methods required by an interface, it is considered to implement that interface.
+type Animal interface {
+	Speak()
+}
+
+type Dog struct {
+	Name string
+}
+
+func Speak(d Dog) {
+	fmt.Println("Woof")
+}
+
+type Cat struct {
+	Name string
+}
+
+func (c Cat) Speak() {
+	fmt.Println("Meow!")
+}
+
+// Using interface in function
+func MakeAnimalSpeak(animal Animal) {
+	animal.Speak()
+}
+
+var newDog = Dog{
+	Name: "Puppy",
+}
+
+var cat = Cat{
+	Name: "Ish",
+}
+
+func lesson5() {
+	MakeAnimalSpeak(newDog)
+}
